be5f824aa6681dc4838e9845ce7e6d3680430dad
tshepo210 <tshepo.thulo@outlook.com>
2026-05-18 22:00:36 +0200

    added pdf validation (#5)

--- glms/Models/Entities/Contract.cs
+++ glms/Models/Entities/Contract.cs
@@ -4,12 +4,12 @@
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
-        public Client Client { get; set; }
+        public Client? Client { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Status { get; set; } // Draft, Active, Expired, On Hold
         public string ServiceLevel { get; set; }
-        public string FilePath { get; set; } // PDF upload
+        public string? FilePath { get; set; } // PDF upload
         public List<ServiceRequest>? ServiceRequests { get; set; }
     }
 }
